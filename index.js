7ed76894a166fe79923ee0945b3b0170c850f942
ortizbran001 <ortizbran001@hotmail.com>
2024-03-19 22:09:46 -0500

    hello world fetch is working

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
+import route from "./routes/employeeRoute.js"
+
+
 
 const app = express()
 app.use(bodyParser.json())
@@ -18,3 +21,5 @@ mongoose.connect(MONGOURL).then( () => {
         console.log(`Server is running on port ${PORT}`)
     })
 }).catch( error => console.log(error))
+
+app.use("/api/employees", route)
